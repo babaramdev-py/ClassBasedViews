bf9120e23ab65531b49011dc8889e1add03002e9
Ved K <vedkorla300@gmail.com>
2023-01-13 22:54:11 +0530

    Debugged a small template error in views.py

--- myapp/views.py
+++ myapp/views.py
@@ -10,5 +10,5 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['injectme'] = 'Injected.'
+        context['inject_me'] = 'Injected.'
         return context
